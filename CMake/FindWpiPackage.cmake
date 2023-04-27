e0d76901c7e05ea9309ae0c4efc053343e86cfe7
Drew Williams <williams.r.drew@gmail.com>
2023-04-27 12:28:46 -0400

    changed macro for generalization

---
+++ CMake/FindWpiPackage.cmake
@@ -0,0 +1,80 @@
+macro(FindWpiPackage packageName version)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/CMake/UrlHelpers.cmake)
+
+GetWpiUrl(${packageName} ${version})
+
+FetchContent_Declare(
+  ${packageName}_headers
+  URL ${HEADER_URL}
+)
+FetchContent_MakeAvailable(${packageName}_headers)
+
+FetchContent_Declare(
+  ${packageName}_libs
+  URL ${LIB_URL}
+)
+
+FetchContent_MakeAvailable(${packageName}_libs)
+
+cmake_print_variables(PATH_SUFFIX)
+
+if(WIN32)
+  find_file(${packageName}_DLL
+    NAMES wpiutil.dll
+    HINTS ${${packageName}_libs_SOURCE_DIR} 
+    PATH_SUFFIXES ${PATH_SUFFIX}
+    REQUIRED
+    NO_DEFAULT_PATH
+    NO_CMAKE_FIND_ROOT_PATH
+  )
+endif()
+
+find_library(${packageName}_LIBRARY 
+  NAMES wpiutil 
+  HINTS ${${packageName}_libs_SOURCE_DIR} 
+  PATH_SUFFIXES ${PATH_SUFFIX}
+  REQUIRED
+  NO_DEFAULT_PATH
+  NO_CMAKE_FIND_ROOT_PATH
+)
+
+set(${packageName}_HEADERS ${${packageName}_headers_SOURCE_DIR})
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(
+  ${packageName} 
+  DEFAULT_MSG
+  ${packageName}_LIBRARY
+  ${packageName}_HEADERS
+)
+
+mark_as_advanced(${packageName}_LIBRARY ${packageName}_HEADERS)
+
+if(${packageName}_FOUND AND NOT TARGET ${packageName}::${packageName})
+  if(GET_SHARED_LIBS)
+    add_library(${packageName}::${packageName} SHARED IMPORTED)
+  else()
+    add_library(${packageName}::${packageName} STATIC IMPORTED)
+  endif()
+
+  if(WIN32)
+    set_target_properties(
+        ${packageName}::${packageName}
+      PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${${packageName}_HEADERS}"
+        IMPORTED_LOCATION ${${packageName}_DLL}
+        IMPORTED_IMPLIB ${${packageName}_LIBRARY}
+    )
+  else()
+    set_target_properties(
+        ${packageName}::${packageName}
+      PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${${packageName}_HEADERS}"
+        IMPORTED_LOCATION ${${packageName}_LIBRARY}
+    )
+  endif()
+endif()
+
+endmacro(FindWpiPackage)
