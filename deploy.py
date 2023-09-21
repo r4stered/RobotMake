0e818e8e6331058c0e2b85e93b4eb015cbf34622
Drew Williams <williams.r.drew@gmail.com>
2023-09-21 18:54:38 -0400

    added lib caching

--- deploy.py
+++ deploy.py
@@ -1,5 +1,7 @@
 import os
 import paramiko
+import hashlib
+import json
 
 
 class MySFTPClient(paramiko.SFTPClient):
@@ -38,6 +40,11 @@ def progress(so_far, total, bar_length=20):
     print(f"Progress: [{arrow}{padding}] {int(fraction*100)}%", end=ending)
 
 
+def sha256sum(filename):
+    with open(filename, "rb", buffering=0) as f:
+        return hashlib.file_digest(f, "sha256").hexdigest()
+
+
 server_usb = "172.22.11.2"
 
 rio_username = "lvuser"
@@ -58,26 +65,44 @@ transport.connect(username=rio_username, password=rio_password)
 sftp = MySFTPClient.from_transport(transport)
 
 print("Copying library files...")
-for filename in os.listdir(lib_dir):
-    print(filename)
-    sftp.put(
-        os.path.join(lib_dir, filename),
-        os.path.join(lib_remote_dir, filename),
-        progress,
-        confirm=True,
-    )
-print("Done copying library files.")
+deploy_cache_dict = {}
+with open("./build/rio_release_shared/deploycache.json", "r+") as deploy_cache_file:
+    old_cache = json.loads(deploy_cache_file.read() or "{}")
+    for filename in os.listdir(lib_dir):
+        file_path_and_name = os.path.join(lib_dir, filename)
+        deploy_cache_dict[file_path_and_name] = sha256sum(file_path_and_name)
+        # if the file doesnt exist in the old cache, or it does exist and is different than before
+        if (old_cache.get(file_path_and_name, None) == None) or (
+            old_cache.get(file_path_and_name, None) != None
+            and old_cache[file_path_and_name] != deploy_cache_dict[file_path_and_name]
+        ):
+            print(file_path_and_name)
+            sftp.put(
+                file_path_and_name,
+                os.path.join(lib_remote_dir, filename),
+                progress,
+                confirm=True,
+            )
+    print("Done copying library files.")
 
 print("Copying deploy folder contents...")
 sftp.put_dir(deploy_dir, deploy_remote_dir)
 print("Done copying deploy files.")
+
+with open("./build/rio_release_shared/deploycache.json", "w") as deploy_cache_file:
+    deploy_cache_file.write(json.dumps(deploy_cache_dict))
+
+print("Removing old robot program...")
+channel = transport.open_session()
+channel.exec_command("rm /home/lvuser/frcUserProgram")
+channel.close()
+print("Done removing old robot program.")
 
 print("Copying robot program...")
 sftp.put(exec_dir, os.path.join(exec_remote_dir, "frcUserProgram"), progress, True)
 print("Done copying robot program.")
 
 sftp.close()
-
 
 print("Starting robot program!")
 # Sets correct permissions and ownership for the robot program
