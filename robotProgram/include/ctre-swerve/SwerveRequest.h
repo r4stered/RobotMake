8c6f34832cd011569afa12fb8141e3a9d6d64fb6
Drew Williams <DrewW@iARx.com>
2023-10-17 14:21:20 -0400

    working on different swerve requests

--- robotProgram/include/ctre-swerve/SwerveRequest.h
+++ robotProgram/include/ctre-swerve/SwerveRequest.h
@@ -9,26 +9,123 @@
 #include <frc/geometry/Pose2d.h>
 #include <ctre-swerve/SwerveModule.h>
 
-namespace RequestTypes {
-struct SwerveControlRequestParameters {
-  frc::SwerveDriveKinematics<4> kinematics;
-  frc::Pose2d currentPose;
-  double timestamp;
-  std::array<frc::Translation2d, 4> swervePositions;
-};
-
-class SwerveRequest {
- public:
-  virtual ~SwerveRequest(){};
-  virtual ctre::phoenix::StatusCode Apply(SwerveControlRequestParameters parameters, std::array<SwerveModule, 4>& modules) = 0;
-};
-
-
-class Idle : public SwerveRequest {
-public:
-  bool isOpenLoop = true;
-  ctre::phoenix::StatusCode Apply(SwerveControlRequestParameters parameters, std::array<SwerveModule, 4>& modules) override {
-    return ctre::phoenix::StatusCode::OK;
+namespace RequestTypes
+{
+  struct SwerveControlRequestParameters
+  {
+    frc::SwerveDriveKinematics<4> kinematics;
+    frc::Pose2d currentPose;
+    double timestamp;
+    std::array<frc::Translation2d, 4> swervePositions;
   };
-};
+
+  class SwerveRequest
+  {
+  public:
+    virtual ~SwerveRequest(){};
+    virtual ctre::phoenix::StatusCode Apply(SwerveControlRequestParameters parameters, std::array<SwerveModule, 4> &modules) = 0;
+  };
+
+  class Idle : public SwerveRequest
+  {
+  public:
+    bool isOpenLoop = true;
+    ctre::phoenix::StatusCode Apply(SwerveControlRequestParameters parameters, std::array<SwerveModule, 4> &modules) override
+    {
+      return ctre::phoenix::StatusCode::OK;
+    };
+  };
+
+  class Brake : public SwerveRequest
+  {
+  public:
+    bool isOpenLoop = true;
+    ctre::phoenix::StatusCode Apply(SwerveControlRequestParameters parameters, std::array<SwerveModule, 4> &modules) override
+    {
+      for (int i = 0; i < modules.size(); i++)
+      {
+        frc::SwerveModuleState state{0_mps, parameters.swervePositions[i].Angle()};
+        modules[i].GoToState(state, isOpenLoop);
+      }
+      return ctre::phoenix::StatusCode::OK;
+    };
+
+    Brake &withIsOpenLoop(bool isOpenLoop)
+    {
+      this->isOpenLoop = isOpenLoop;
+      return *this;
+    }
+  };
+
+  class FieldCentric : public SwerveRequest
+  {
+  public:
+    units::meters_per_second_t velocityX;
+    units::meters_per_second_t velocityY;
+    units::radians_per_second_t rotationalRate;
+    units::meters_per_second_t deadband = 0_mps;
+    units::radians_per_second_t rotationalDeadband = 0_rad_per_s;
+    bool isOpenLoop = true;
+    ctre::phoenix::StatusCode Apply(SwerveControlRequestParameters parameters, std::array<SwerveModule, 4> &modules) override
+    {
+      units::meters_per_second_t toApplyX = velocityX;
+      units::meters_per_second_t toApplyY = velocityY;
+      units::radians_per_second_t toApplyOmega = rotationalRate;
+      if (units::math::sqrt(toApplyX * toApplyX + toApplyY * toApplyY) < deadband)
+      {
+        toApplyX = 0_mps;
+        toApplyY = 0_mps;
+      }
+      if (units::math::abs(toApplyOmega) < rotationalDeadband)
+      {
+        toApplyOmega = 0_rad_per_s;
+      }
+      frc::ChassisSpeeds speeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(toApplyX, toApplyY, toApplyOmega, parameters.currentPose.Rotation());
+      auto states = parameters.kinematics.ToSwerveModuleStates(speeds, frc::Translation2d{});
+      for (int i = 0; i < modules.size(); i++)
+      {
+        modules[i].GoToState(states[i], isOpenLoop);
+      }
+      return ctre::phoenix::StatusCode::OK;
+    };
+
+    FieldCentric &withIsOpenLoop(bool isOpenLoop)
+    {
+      this->isOpenLoop = isOpenLoop;
+      return *this;
+    }
+
+    FieldCentric &withVelocityX(units::meters_per_second_t velX)
+    {
+      this->velocityX = velX;
+      return *this;
+    }
+
+    FieldCentric &withVelocityY(units::meters_per_second_t velY)
+    {
+      this->velocityY = velY;
+      return *this;
+    }
+
+    FieldCentric &withRotationalRate(units::radians_per_second_t omega)
+    {
+      this->rotationalRate = omega;
+      return *this;
+    }
+
+    FieldCentric &withDeadband(units::meters_per_second_t deadband)
+    {
+      this->deadband = deadband;
+      return *this;
+    }
+
+    FieldCentric &withRotationalDeadband(units::radians_per_second_t rotDeadband)
+    {
+      this->rotationalDeadband = rotDeadband;
+      return *this;
+    }
+
+  private:
+    std::array<frc::SwerveModuleState, 4> lastAppliedState{};
+  };
 }
---
+++ robotProgram/src/ctre-swerve/CTREPIDController.cpp
@@ -0,0 +1,201 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "ctre-swerve/CTREPIDController.h"
+
+#include <algorithm>
+#include <cmath>
+
+#include <wpi/sendable/SendableBuilder.h>
+#include <wpi/sendable/SendableRegistry.h>
+
+#include "frc/MathUtil.h"
+#include "wpimath/MathShared.h"
+
+using namespace frc;
+
+CTREPIDController::CTREPIDController(double Kp, double Ki, double Kd)
+    : m_Kp(Kp), m_Ki(Ki), m_Kd(Kd)
+{
+    static int instances = 0;
+    instances++;
+
+    wpi::math::MathSharedStore::ReportUsage(
+        wpi::math::MathUsageId::kController_PIDController2, instances);
+    wpi::SendableRegistry::Add(this, "CTREPIDController", instances);
+}
+
+void CTREPIDController::SetPID(double Kp, double Ki, double Kd)
+{
+    m_Kp = Kp;
+    m_Ki = Ki;
+    m_Kd = Kd;
+}
+
+void CTREPIDController::SetP(double Kp)
+{
+    m_Kp = Kp;
+}
+
+void CTREPIDController::SetI(double Ki)
+{
+    m_Ki = Ki;
+}
+
+void CTREPIDController::SetD(double Kd)
+{
+    m_Kd = Kd;
+}
+
+double CTREPIDController::GetP() const
+{
+    return m_Kp;
+}
+
+double CTREPIDController::GetI() const
+{
+    return m_Ki;
+}
+
+double CTREPIDController::GetD() const
+{
+    return m_Kd;
+}
+
+double CTREPIDController::GetPositionTolerance() const
+{
+    return m_positionTolerance;
+}
+
+double CTREPIDController::GetVelocityTolerance() const
+{
+    return m_velocityTolerance;
+}
+
+double CTREPIDController::GetSetpoint() const
+{
+    return m_setpoint;
+}
+
+bool CTREPIDController::AtSetpoint() const
+{
+    return m_haveMeasurement && m_haveSetpoint &&
+           std::abs(m_positionError) < m_positionTolerance &&
+           std::abs(m_velocityError) < m_velocityTolerance;
+}
+
+void CTREPIDController::EnableContinuousInput(double minimumInput,
+                                              double maximumInput)
+{
+    m_continuous = true;
+    m_minimumInput = minimumInput;
+    m_maximumInput = maximumInput;
+}
+
+void CTREPIDController::DisableContinuousInput()
+{
+    m_continuous = false;
+}
+
+bool CTREPIDController::IsContinuousInputEnabled() const
+{
+    return m_continuous;
+}
+
+void CTREPIDController::SetIntegratorRange(double minimumIntegral,
+                                           double maximumIntegral)
+{
+    m_minimumIntegral = minimumIntegral;
+    m_maximumIntegral = maximumIntegral;
+}
+
+void CTREPIDController::SetTolerance(double positionTolerance,
+                                     double velocityTolerance)
+{
+    m_positionTolerance = positionTolerance;
+    m_velocityTolerance = velocityTolerance;
+}
+
+double CTREPIDController::GetPositionError() const
+{
+    return m_positionError;
+}
+
+double CTREPIDController::GetVelocityError() const
+{
+    return m_velocityError;
+}
+
+double CTREPIDController::Calculate(double measurement, double setpoint, units::second_t currentTimestamp)
+{
+    m_setpoint = setpoint;
+    m_haveSetpoint = true;
+    m_measurement = measurement;
+    m_prevError = m_positionError;
+    m_haveMeasurement = true;
+
+    units::second_t thisPeriod = currentTimestamp - m_lastTimestamp;
+    m_lastTimestamp = currentTimestamp;
+
+    if (m_continuous)
+    {
+        double errorBound = (m_maximumInput - m_minimumInput) / 2.0;
+        m_positionError =
+            InputModulus(m_setpoint - m_measurement, -errorBound, errorBound);
+    }
+    else
+    {
+        m_positionError = m_setpoint - m_measurement;
+    }
+
+    m_velocityError = (m_positionError - m_prevError) / thisPeriod.value();
+
+    if (m_Ki != 0)
+    {
+        m_totalError =
+            std::clamp(m_totalError + m_positionError * thisPeriod.value(),
+                       m_minimumIntegral / m_Ki, m_maximumIntegral / m_Ki);
+    }
+
+    m_lastAppliedOutput = m_Kp * m_positionError + m_Ki * m_totalError + m_Kd * m_velocityError;
+    return m_lastAppliedOutput;
+}
+
+void CTREPIDController::Reset()
+{
+    m_positionError = 0;
+    m_prevError = 0;
+    m_totalError = 0;
+    m_velocityError = 0;
+    m_haveMeasurement = false;
+}
+
+double CTREPIDController::GetLastAppliedOutput()
+{
+    return m_lastAppliedOutput;
+}
+
+void CTREPIDController::InitSendable(wpi::SendableBuilder &builder)
+{
+    builder.SetSmartDashboardType("CTREPIDController");
+    builder.AddDoubleProperty(
+        "p", [this]
+        { return GetP(); },
+        [this](double value)
+        { SetP(value); });
+    builder.AddDoubleProperty(
+        "i", [this]
+        { return GetI(); },
+        [this](double value)
+        { SetI(value); });
+    builder.AddDoubleProperty(
+        "d", [this]
+        { return GetD(); },
+        [this](double value)
+        { SetD(value); });
+    builder.AddDoubleProperty(
+        "setpoint", [this]
+        { return GetSetpoint(); },
+        nullptr);
+}
