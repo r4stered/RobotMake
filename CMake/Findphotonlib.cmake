ff90275585808f889d3a35d6ffbaf8d0cf2aa1dc
Drew Williams <DrewW@iARx.com>
2024-04-16 10:43:06 -0400

    added photontargeting

--- CMake/Findphotonlib.cmake
+++ CMake/Findphotonlib.cmake
@@ -1,3 +1,4 @@
 include(${CMAKE_CURRENT_SOURCE_DIR}/CMake/FindWpiPackage.cmake)
 
 FindWpiPackage(photonlib "" "v2024.3.1" "" NO NO GetPhotonvisionUrl)
+FindWpiPackage(photontargeting "" "v2024.3.1" "" NO NO GetPhotonvisionUrl)
